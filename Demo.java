135fcd98946452686565b07c2d5530d11ea3eb8a
Arich708 <githubuser708@gmail.com>
2024-12-26 11:38:26 +0530

    Add files via upload

---
+++ Demo.java
@@ -0,0 +1,16 @@
+
+public class Demo {
+	
+
+	public static void main(String[] args) {
+		
+		System.out.println("Hello");
+		for(int i=1;i<=5;i++)
+		{
+			System.out.print(" "+i);
+		}
+		
+		System.out.println("\nCompleted");
+	}
+
+}
